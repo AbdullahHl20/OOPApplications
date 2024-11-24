f78acaf7c497228e5c3ef74691afa00f98d09c53
Abdallh Alhelel <abdallh20hl@gmail.com>
2024-11-24 21:03:14 +0300

    add clsUpdateClientScreen

--- OOPApplcation/clsMainScreen.h
+++ OOPApplcation/clsMainScreen.h
@@ -5,6 +5,7 @@
 #include "clsClientListScreen.h"
 #include "clsAddNewClientScreen.h"
 #include "clsDeleteClientScreen.h"
+#include "clsUpdateClientScreen.h"
 
 using namespace std;
 
@@ -39,6 +40,7 @@ private:
         //  cout << "\nClient List Screen Will be here...\n";
         clsClientListScreen::ShowClientsList();
 
+
     }
 
     static void _ShowAddNewClientsScreen()
@@ -57,7 +59,9 @@ private:
 
     static void _ShowUpdateClientScreen()
     {
-        cout << "\nUpdate Client Screen Will be here...\n";
+        //cout << "\nUpdate Client Screen Will be here...\n";
+        clsUpdateClientScreen::ShowUpdateClientScreen();
+
 
     }
 
