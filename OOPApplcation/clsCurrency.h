d9402978ff9a12c193ce0e6030ee856de5727ac1
Abdallh Alhelel <abdallh20hl@gmail.com>
2024-12-03 19:35:57 +0300

    add  clsCurrencyCalculatorScreen

--- OOPApplcation/clsCurrency.h
+++ OOPApplcation/clsCurrency.h
@@ -81,7 +81,7 @@ private:
         if (MyFile.is_open())
         {
 
-            for (clsCurrency& C : vCurrencys)
+            for (clsCurrency C : vCurrencys)
             {
                 DataLine = _ConverCurrencyObjectToLine(C);
                 MyFile << DataLine << endl;
@@ -239,6 +239,25 @@ public:
     {
         return _LoadCurrencysDataFromFile();
     }
+
+    float ConvertToUSD(float Amount)
+    {
+        return (float)(Amount / Rate());
+    }
+
+    float ConvertToOtherCurrency(float Amount, clsCurrency Currency2)
+    {
+        float AmountInUSD = ConvertToUSD(Amount);
+
+        if (Currency2.CurrencyCode() == "USD")
+        {
+            return AmountInUSD;
+        }
+
+        return (float)(AmountInUSD * Currency2.Rate());
+
+    }
+
 };
 
 
